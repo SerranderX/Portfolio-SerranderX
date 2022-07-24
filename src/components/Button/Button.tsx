7c4506f13f893a40890a6aaf75715c6e8d7b3455
SerranderX <felipe.diaz@xintec.cl>
2022-07-24 13:30:57 -0400

    :bug: [FIX] bug in deployment

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
+import { ButtonProps } from '@interfaces/buttona.interface';
 import styles from '@styles/button.module.scss';
-import { ButtonProps } from '@interfaces/button.interface';
 
 export const Button: React.FC<ButtonProps> = ({text, classes = "", handleButton}) => {
     return (
