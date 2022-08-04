780efbed79a13ab6c5168b1c9191cc7837946395
SerranderX <felipe.diaz@xintec.cl>
2022-08-04 11:03:31 -0400

    :lipstick: [Modify] Experience ionterval

--- src/containers/Experience/Experience.tsx
+++ src/containers/Experience/Experience.tsx
@@ -32,7 +32,7 @@ export const Experience: React.FC<ExperienceProps> = ({windowDimenions}) => {
                 <h1>{experience.title}</h1>
                 <p>{experience.description}</p>
             </article>
-            <Carousel items={experienceItems} visibleItems={visibleItemsAdjust} containerClass={styles.icons} autoPlay={true} interval={30000} />
+            <Carousel items={experienceItems} visibleItems={visibleItemsAdjust} containerClass={styles.icons} autoPlay={true} interval={3000} />
         </section>
     );
 };
